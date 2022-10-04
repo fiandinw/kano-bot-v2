bc3f0b466fd9cd01efaadf6cb437ac9cbeb9a1c8
fiandinw <alifiandinursanni@gmail.com>
2022-10-05 03:16:23 +0700

    status

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 // about the middleware, please refer to doc
 app.post("/line", line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
+    .then((result) => res.status(200).json(result))
     .catch((err) => {
       console.error(err);
       res.status(500).end();
